248a3897a8f51397b264602625c2f8d2521a9973
jaggaer-sfelaco <90247417+jaggaer-sfelaco@users.noreply.github.com>
2025-02-24 18:10:50 +0100

    fix tools.py

--- tools/tools.py
+++ tools/tools.py
@@ -1,7 +1,7 @@
-from langchain_community.tools.tavily_search import TavilySearchResults
+from langchain_community.tools import TavilySearchResults
 
 def get_profile_url_tavily(name: str, context : dict) -> str:
     context['call_count'] += 1
-    search = TavilySearchResults(max_results=context['call_count'] *5)
-    res = search.run(name)
+    search = TavilySearchResults(max_results=5 * context['call_count'])
+    res = search.invoke({'query': name})
     return res
