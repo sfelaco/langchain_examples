2300778a6ebec86a605327e8025b2bc3273fa6c4
jaggaer-sfelaco <90247417+jaggaer-sfelaco@users.noreply.github.com>
2025-02-28 17:05:10 +0100

    implementation reAct agent

--- tools/tools.py
+++
@@ -1,6 +0,0 @@
-from langchain_community.tools import TavilySearchResults
-
-def get_profile_url_tavily(name: str) -> str:
-    search = TavilySearchResults()
-    res = search.invoke({'query': name})
-    return res[0]["url"]
